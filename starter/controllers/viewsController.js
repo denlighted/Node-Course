6fb8b197417949ed8c696317179486ff58609b98
Denys Harkovenko <dgarkovenko10@gmail.com>
2025-08-05 17:51:39 +0300

    Implemented stripe messages 2

--- starter/controllers/viewsController.js
+++ starter/controllers/viewsController.js
@@ -73,6 +73,6 @@ exports.alerts = (req,res,next)=>{
   const { alert } = req.query;
   if(alert === 'booking'){
     res.locals.alert = "Your booking was successful! Please check your email for confirmation. If your booking doesn't show up here immediately, please come back later";
-
   }
+  next();
 }
